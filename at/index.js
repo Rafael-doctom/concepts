31d1b2068c29e4712999ff8f02334a75c2833f79
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2023-01-31 08:45:13 -0300

    at

---
+++ at/index.js
@@ -0,0 +1,13 @@
+// index to return item referring last index
+//  accepted netgative numbers
+
+let numbers = [1, 2, 0, 4, 7, 10]
+
+let index = 5
+
+console.log(`index: ${index} and value ${numbers.at(index)}`)
+
+
+let negative = -2
+
+console.log(`index negative ${negative} and value ${numbers.at(negative)}`)
