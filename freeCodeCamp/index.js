10e05c5261e56731274bb9ec24c4008e8d45ebec
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2023-01-31 09:31:56 -0300

    exercise

---
+++ freeCodeCamp/index.js
@@ -0,0 +1,33 @@
+// using comment in your JS
+
+// create a array & return in your console
+
+// let numbers = [1, 2, 3, 4, 5]
+// console.log(numbers)
+
+// using var, let, const
+
+// var test1 = 'global'
+// let test2 = 'block scope'
+// let test3 = 'scope fixe'
+
+// function global() {
+//     console.log(test1)
+// }
+
+// function blockScope() {
+//     console.log(test2)
+// }
+
+// function scopeFixe() {
+//     console.log(test3)
+// }
+
+// global(test1)
+// blockScope(test2)
+// scopeFixe(test3)
+
+// using block quote
+
+// let creatorOfLinux = 'my favorite SO is Linux... and your creator is \"Linus Torvalds\"'
+// console.log(creatorOfLinux)
